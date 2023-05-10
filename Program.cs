e3d7a8f41dc5f1a1b2a54725a0a5f3f51ffb165a
LuisMarioGarciaj <GCL0033412@est.univalle.edu>
2023-05-10 14:32:15 -0400

    Holamundo2

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
-﻿namespace ConsoleApp1
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-
+            int n,n2,suma;
+            Console.WriteLine("Ingrese un numero");
+            n = int.Parse(Console.ReadLine());
+            n2 = int.Parse(Console.ReadLine());
+            suma = n + n2;
+            Console.WriteLine("LA SUMA ES " + suma);
         }
     }
 }
